34bbf29779a8da9371beb1ff3939a314b60d7d7c
ocean_rey <x7flux@gmail.com>
2022-04-23 03:23:19 +0300

    improved request validation

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -12,30 +12,37 @@ import { isNewEmployee } from "../validators";
 
 const router = Router()
 
-router.post("/register-admin", requireClean, body("password").isLength({ min: 6 }), body("empNumber").isString().isLength({ max: 4, min: 4 }), async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+router.post("/register-admin",
+    requireClean,
+    body("password").isLength({ min: 6 }).withMessage("must be at least 6 chars long"),
+    body("empNumber").isNumeric().withMessage("must be numeric string")
+        .isLength({ max: 4, min: 4 }).withMessage("must be 4 chars long"),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const { empNumber, password, name } = req.body;
+            const jti = uuidv4()
+            // if there is a password; it must be > 5 char long
+            const user = await createUser({ empNumber, password, role: "Admin", name: name ?? null })
+            const { accessToken, refreshToken } = generateTokens(user, jti)
+            await whiteListRefreshToken({ jti, refreshToken, userId: user.id })
+            res.status(200).json({ accessToken, refreshToken }).send()
+        } catch (error) {
+            console.error(error)
+            return res.status(500).send()
         }
-        const { empNumber, password, name } = req.body;
-        const jti = uuidv4()
-        // if there is a password; it must be > 5 char long
-        const user = await createUser({ empNumber, password, role: "Admin", name: name ?? null })
-        const { accessToken, refreshToken } = generateTokens(user, jti)
-        await whiteListRefreshToken({ jti, refreshToken, userId: user.id })
-        res.status(200).json({ accessToken, refreshToken }).send()
-    } catch (error) {
-        console.error(error)
-        return res.status(500).send()
-    }
-})
+    })
 
 router.post("/create-user",
     requireRole,
     body("password").isLength({ min: 6 }),
-    body("empNumber").isNumeric().custom(isNewEmployee),
-    body("role").isString().isIn(["Admin", "Employee"]),
+    body("role").isString().isIn(["Admin", "Employee"]).withMessage(`must be one of "Admin, Employee"`),
+    body("empNumber").isNumeric().withMessage("must be numeric string")
+        .isLength({ min: 4, max: 4 }).withMessage("must be a numeric string with length 4")
+        .custom(isNewEmployee).withMessage("user already exists"),
     async (req, res) => {
         try {
             const errors = validationResult(req);
@@ -54,30 +61,37 @@ router.post("/create-user",
         }
     })
 
-router.post("/login", body("empNumber").isNumeric(), body("password"),  async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+
+router.post("/login",
+    body("empNumber").isNumeric().isLength({ min: 4, max: 4 }),
+    body("password").isLength({ min: 6 }),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                // for the login route, i decided against sending back information on request validation failure
+                // the reasoning for this is to prevent attackers from gaining information on possible login combinations
+                // that said; we can still safetly check for validation errors, saving time by not bothering to check the db
+                return res.status(403).send("Invalid credentials");
+            }
+            const { empNumber, password } = req.body;
+            const existingUser = await findUserByempNumber(empNumber);
+            if (!existingUser) {
+                return res.status(403).send("Invalid credentials");
+            }
+            const validPassword = await bcrypt.compare(password, existingUser.password);
+            if (!validPassword) {
+                return res.status(403).send("Invalid credentials");
+            }
+            await revokeUserTokens(existingUser.id); // kill existing sessions if any
+            const jti = uuidv4();
+            const { accessToken, refreshToken } = generateTokens(existingUser, jti);
+            await whiteListRefreshToken({ jti, refreshToken, userId: existingUser.id })
+            res.status(200).json({ accessToken, refreshToken }).send()
+        } catch (error) {
+            res.status(500).send(error)
         }
-        const { empNumber, password } = req.body;
-        const existingUser = await findUserByempNumber(empNumber);
-        if (!existingUser) {
-            return res.status(403).send("Invalid credentials");
-        }
-        const validPassword = await bcrypt.compare(password, existingUser.password);
-        if (!validPassword) {
-            return res.status(403).send("Invalid credentials");
-        }
-        await revokeUserTokens(existingUser.id); // kill existing sessions if any
-        const jti = uuidv4();
-        const { accessToken, refreshToken } = generateTokens(existingUser, jti);
-        await whiteListRefreshToken({ jti, refreshToken, userId: existingUser.id })
-        res.status(200).json({ accessToken, refreshToken }).send()
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
+    })
 
 router.post("/refreshToken", async (req, res) => {
     try {
