03138f6ef1b437c4f67ae2dacd3a9b1ee733dc03
ocean_rey <x7flux@gmail.com>
2022-04-23 05:59:55 +0300

    added table schemas for reservations and tables

--- api/src/utils/db.ts
+++ api/src/utils/db.ts
@@ -2,3 +2,5 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 export const Users = db.user;
 export const RefreshTokens = db.refreshToken;
+export const Tables = db.table;
+export const Reservations = db.reservation;
