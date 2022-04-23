8f388ce9d7878dd24f8a5ca8375e16520247ee1c
ocean_rey <x7flux@gmail.com>
2022-04-23 04:11:04 +0300

    jwt verification in requireRole middleware

--- api/src/middleware.ts
+++ api/src/middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { Users } from "./utils/db";
+import jwt from "jsonwebtoken"
 
 
 // TODO
@@ -11,6 +12,9 @@ export const requireRole = async (req: Request, res: Response, next: NextFunctio
     try {
         // it would be faster than redis to simply store the role in the jwt
         // but that wasn't the task requirment so i'll do it this way
+        const token = authorization.split(' ')[1];
+        const payload = jwt.verify(token, process.env.JWT_ACCESS_SECRET) as {userId: string};
+        // getting this far means we have a valid token, get the role from redis
     } catch (error) {
         return res.status(401).send()
     }
